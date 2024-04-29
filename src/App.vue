e2a09627a72c665014551f38a882d039c31bb0f4
chenw <532836459@qq.com>
2024-04-29 14:45:28 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,20 @@
 <script setup>
 import useSettingsStore from '@/store/modules/settings'
 import useConfigStore from "@/store/modules/config.js";
-const configStore = useConfigStore()
 import { handleThemeStyle } from '@/utils/theme'
+const configStore = useConfigStore()
 
-onMounted(() => {
+onMounted(async () => {
+
+  //初始化网站配置
+  await useConfigStore().getConfig();
+  let favicon = document.querySelector('link[rel="icon"]');
+  favicon.href = configStore.favicon;
+  document.title = configStore.name;
+
   nextTick(() => {
     // 初始化主题样式
     handleThemeStyle(useSettingsStore().theme);
-    //初始化网站配置
-    useConfigStore().getConfig();
-    // 设置网站ico
-    let favicon = document.querySelector('link[rel="icon"]');
-    if (favicon) {
-      favicon.href = configStore.favicon;
-      return;
-    }
-    favicon = document.createElement('link');
-    favicon.rel = 'icon';
-    favicon.href = configStore.favicon;
-    document.head.appendChild(favicon);
-    //设置网站title
-
   })
 })
 </script>
